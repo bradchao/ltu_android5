237b5d4993a7f63e2b9cb4999437ef00561f876d
brad <brad@brad.tw>
2017-12-13 14:29:21 +0800

    201712131429

--- app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -12,11 +13,17 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText account, passwd, realname;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        account = (EditText)findViewById(R.id.account);
+        passwd = (EditText)findViewById(R.id.passwd);
+        realname = (EditText)findViewById(R.id.realname);
+
     }
 
     public void test1(View view){
@@ -43,5 +50,32 @@ public class MainActivity extends AppCompatActivity {
             }
         }.start();
     }
+
+    public void test2(View view){
+        new Thread(){
+            @Override
+            public void run() {
+                try {
+                    String urlString = "http://120.108.137.125/ltu/ltu03.php?account="
+                            + account.getText() +
+                            "&passwd=" + passwd.getText()
+                            + "&realname=" + realname.getText();
+                    URL url = new URL(urlString);
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.connect();
+
+                    BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                    String line = br.readLine();
+                    br.close();
+
+                    Log.v("brad", line);
+
+                } catch (Exception e) {
+                    Log.v("brad", e.getLocalizedMessage());
+                }
+            }
+        }.start();
+    }
+
 
 }
